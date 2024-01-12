8d70d0c79425f55946352aa9a8fc0c015f36e407
karolparra <156379200+karolparra@users.noreply.github.com>
2024-01-12 15:47:36 -0300

    Update env.rb
    Resolvendo problema de ReadTimeOut.

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -6,6 +6,5 @@ require 'site_prism'
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.app_host = 'https://blogdoagi.com.br/'
-  config.default_max_wait_time = 5
   Capybara.current_session.driver.browser.manage.window.resize_to(1920, 1080)
 end
